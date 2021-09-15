31358b2f452772aa6437ae2752bd819e2e53813e
Sonika <sonikapoudyal5@gmail.com>
2021-09-15 11:55:13 -0400

    header page set

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Post from './pages/Post';
+
+
+function App() {
+    return (
+        <div>
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/signup">
+                        <Signup />
+                    </Route>
+                    <Route path="/profile">
+                        <Profile />
+                    </Route>
+                    <Route path="/post">
+                        <Post />
+                    </Route>
+                </Switch>
+            </Router>
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
