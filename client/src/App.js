ee93f753710e4b8646f8f394214590f246eac619
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-09-15 20:37:32 -0400

    remove express routes and imported apollo into client side. updated app.js for createHTTPLink, setContect, apolloclient

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,67 @@
-import React from 'react';
+import React from "react";
 import {
-    HashRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Post from './pages/Post';
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Post from "./pages/Post";
 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-    return (
-        <div>
-            <Router>
-                <Header />
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/login">
-                        <Login />
-                    </Route>
-                    <Route path="/signup">
-                        <Signup />
-                    </Route>
-                    <Route path="/profile">
-                        <Profile />
-                    </Route>
-                    <Route path="/post">
-                        <Post />
-                    </Route>
-                </Switch>
-            </Router>
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/post">
+              <Post />
+            </Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </ApolloProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+// need to update queries based off new schema from Reuben
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+    }
+  }
+`;
