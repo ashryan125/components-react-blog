adbcd790ca5a3c16b5eb6511150848e2c37506c4
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-10-03 21:32:42 -0400

    corrected element heirarchy on comments

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const CommentList = ({ comments }) => {
-    return(
-        <div className="card mb-3 border-0">
-            <div className="card-header bg-secondary">
-                <span className="text-light" style={{ fontWeight: 700 }}>Comments</span>
-            </div>
-            <div className="card-body">
-                {comments &&
-                    comments.map(comment => (
-                        <p className="comment-background" key={comment._id}>
-                            {comment.commentBody}
-                            <div>
-                            <Link to={`/profile/${comment.username}`} className='comment-poster'>
-                                {comment.username} on {comment.createdAt}
-                            </Link>
-                            </div>
-                        </p>
-                    ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="card mb-3 border-0">
+      <div className="card-header bg-secondary">
+        <span className="text-light" style={{ fontWeight: 700 }}>
+          Comments
+        </span>
+      </div>
+      <div className="card-body">
+        {comments &&
+          comments.map((comment) => (
+            <p className="comment-background" key={comment._id}>
+              {comment.commentBody}
+              <br></br>
+              <Link
+                to={`/profile/${comment.username}`}
+                className="comment-poster"
+              >
+                {comment.username} on {comment.createdAt}
+              </Link>
+            </p>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default CommentList;
